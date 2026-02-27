8e666b6956b9ad283611fca01144279bde2121d4
PREETAM PRAJAPATI <preetamp765@gmail.com>
2026-02-27 12:06:30 +0530

    commit2

---
+++ job port/TallentSync/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./AboutUs/**/*.html",
+    "./ContactUs/**/*.html",
+    "./PostAJob/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
